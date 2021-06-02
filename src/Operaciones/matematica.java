3d1612aa0a34299275357191b79a508d836899db
Jean Jacome <jeanjacome5055@hotmail.com>
2021-06-02 10:57:34 -0500

    java v1

---
+++ src/Operaciones/matematica.java
@@ -0,0 +1,13 @@
+package operaciones;
+
+public class matematica {
+
+    int a;
+    int b;
+
+    public void sumar() {
+        int resultado = a + b;
+        System.out.println("El resultado es" + resultado);
+    }
+
+}
